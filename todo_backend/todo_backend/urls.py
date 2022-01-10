2b48049be3c4cb8991bc0ff3f2ad22d0264402ca
Dalton Ronan <dronan@ualberta.ca>
2022-01-09 22:04:47 -0700

    add drf and endpoint

--- todo_backend/todo_backend/urls.py
+++ todo_backend/todo_backend/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'todo', views.TodoView, 'todo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
