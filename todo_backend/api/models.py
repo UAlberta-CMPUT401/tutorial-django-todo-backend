42539799052f435824942bd664b0f28784356bad
Dalton Ronan <dronan@ualberta.ca>
2022-01-09 21:52:41 -0700

    register admin site

--- todo_backend/api/models.py
+++ todo_backend/api/models.py
@@ -4,3 +4,6 @@ class Todo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
