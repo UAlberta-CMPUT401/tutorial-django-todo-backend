99845d709d9b4e134c8db321a9ffc35d49d58c57
dijonron <37709233+dijonron@users.noreply.github.com>
2022-01-09 21:41:06 -0700

    Update settings.py

--- todo_backend/todo_backend/settings.py
+++ todo_backend/todo_backend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'api.apps.TodoConfig',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
