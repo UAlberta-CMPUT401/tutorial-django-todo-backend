dc62f5af7856aa4ae772dbf76fd94aa04a6b0396
Dalton Ronan <dronan@ualberta.ca>
2022-01-09 21:46:29 -0700

    create todo model

--- todo_backend/api/models.py
+++ todo_backend/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
